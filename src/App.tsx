755b07eaf4af137c39904de18856f58f44e9a265
Ciki <aandjelkovic@sonovate.com>
2017-04-25 13:26:11 +0100

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { AppRootBooty } from "./Test";
+
+ReactDOM.render(
+    <AppRootBooty/>,
+    document.getElementById("app")
+);
---
+++ src/Test.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+
+export const AppRootBooty = () => <h1>What app stud</h1>;
